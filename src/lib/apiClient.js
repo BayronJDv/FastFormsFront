00e5789153423f48c8946ce65e8ea89c9d8fbd91
morfeo <bayron.jojoa.dv@gmail.com>
2026-05-12 16:05:59 -0500

    si

--- src/lib/apiClient.js
+++ src/lib/apiClient.js
@@ -1,7 +1,7 @@
 import { getToken } from "./gettoken";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1";
+  import.meta.env.VITE_API_URL+"/api/v1" || "http://localhost:8000/api/v1";
 
 async function request(endpoint, options = {}) {
   const token = await getToken();
